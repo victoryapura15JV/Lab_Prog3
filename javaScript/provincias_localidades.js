a8b87d9adfe9b4b241278365ed54af87061d6477
judithgriseldaescalante-svg <judithgriseldaescalante@gmail.com>
2026-09-13 01:45:24 -0300

    creando carpeta de javascript

---
+++ javaScript/provincias_localidades.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const provinciasYlocalidades = {
+        "catamarca": ["Capital","Valle Viejo","Fray Mamerto Esquiu","Santa Maria"],
+        "salta" : ["Capital","Cafayate","Tartagal","San Lorenzo"],
+        "tucuman" : ["Capital","Tafi del Valle","Yerba Buena"],
+        "otra" : ["Otra localidad"]
+    }
+    document.addEventListener("change",()=>{
+        let formulario = document.getElementById("")
+    })
+})
